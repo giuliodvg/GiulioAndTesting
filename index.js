3fbcc78c942d7d750bc34e2c993ef8fabbed752b
giuliodvg <109850410+giuliodvg@users.noreply.github.com>
2022-09-17 12:52:58 +0200

    node .

--- index.js
+++ index.js
@@ -158,3 +158,35 @@ client.on("messageCreate", message => {
         message.channel.send({ content: "Clicca sul bottone per aprire un ticket", components: [row] })
     }
 })
+
+client.on("interactionCreate", interaction => {
+    if (interaction.customId == "apriTicket") {
+         interaction.deferUpdate()
+        if (interaction.guild.channels.cache.find(canale => canale.topic == "User ID:" `${interaction.user.id}`)) {
+            interaction.user.send("Hai gia un ticket aperto").catch(() => { })
+            return
+        }
+        interaction.guild.channels.create(interaction.user.username, {
+            type: "text",
+            topic: `User ID: \${interaction.user.id}`,
+            parent: "idCategoria", //Settare la categoria,
+            permissionOverwrites: [
+                {
+                    id: interaction.guild.id,
+                    deny: ["VIEW_CHANNEL"]
+                },
+                {
+                    id: interaction.user.id,
+                    allow: ["VIEW_CHANNEL"]
+                },
+                { //Aggiungere altri "blocchi" se si vogliono dare permessi anche a ruoli o utenti
+                    id: "idRuolo",
+                    allow: ["VIEW_CHANNEL"]
+                }
+            ]
+        }).then(canale => {
+            canale.send("Grazie per aver aperto un ticket")
+        })
+    }
+})
+
